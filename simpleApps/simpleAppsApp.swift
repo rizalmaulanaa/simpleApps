4401543763d136c8fb0bd5dbd2ec281ed399cf4c
Rizal Maulana <rizal.0951@gmail.com>
2024-04-17 15:01:09 +0700

    Initial Commit

---
+++ simpleApps/simpleAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppsApp.swift
+//  simpleApps
+//
+//  Created by Rizal Maulana on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
